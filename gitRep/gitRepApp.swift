44587b520717abab2749febc25db0421d908adcd
Diva Jain <divamakesmusic@gmail.com>
2023-06-26 15:28:49 -0400

    Initial Commit

---
+++ gitRep/gitRepApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitRepApp.swift
+//  gitRep
+//
+//  Created by Scholar on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct gitRepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
